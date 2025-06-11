5f585a2714ffc5fd4c89482bdb90a7944f902af1
miswah <miswah.md@gmail.com>
2025-06-12 00:46:03 +0530

    (feat): Support for mock api uses #2

--- src/main/java/com/miswah/apiorbit/repository/MockApiRepository.java
+++ src/main/java/com/miswah/apiorbit/repository/MockApiRepository.java
@@ -1,10 +1,17 @@
 package com.miswah.apiorbit.repository;
 
+import com.miswah.apiorbit.enums.HttpMethods;
+import com.miswah.apiorbit.model.ApiDefinitionModel;
+import com.miswah.apiorbit.model.ApiVersionModel;
 import com.miswah.apiorbit.model.MockApiModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MockApiRepository extends JpaRepository<MockApiModel, Long> {
 
+    Optional<MockApiModel> findByApiDefinitionModel(ApiDefinitionModel id);
+    Optional<MockApiModel> findByApiDefinitionModelAndHttpMethods(ApiDefinitionModel model, HttpMethods httpMethods);
 }
