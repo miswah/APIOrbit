a4e7d529ada2fdddcfbb62318f0c6c8547d59f21
miswah <miswah.md@gmail.com>
2025-11-21 10:38:28 +0530

    (temp): cors filters removed

--- APIOrbit-API/src/main/java/com/miswah/apiorbit/controller/AuthenticationController.java
+++ APIOrbit-API/src/main/java/com/miswah/apiorbit/controller/AuthenticationController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api/auth")
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
