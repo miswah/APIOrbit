b177cfe8b8ae9ad0aac20fd7fa6221116e444549
miswah <miswah.md@gmail.com>
2026-01-03 17:40:44 +0530

    (feat): activity log api

--- APIOrbit-API/src/main/java/com/miswah/apiorbit/service/ActivityLogService.java
+++ APIOrbit-API/src/main/java/com/miswah/apiorbit/service/ActivityLogService.java
@@ -1,7 +1,11 @@
 package com.miswah.apiorbit.service;
 
+import com.miswah.apiorbit.dto.response.ActivityLogResponseDTO;
 import com.miswah.apiorbit.model.ActivityLogModel;
+
+import java.util.List;
 
 public interface ActivityLogService {
     public void save(ActivityLogModel log);
+    public List<ActivityLogResponseDTO> getLogs();
 }
